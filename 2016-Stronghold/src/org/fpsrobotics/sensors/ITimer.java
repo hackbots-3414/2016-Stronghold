f2ccfe218c3873e297fbbbf8cda66873de9955b6
Kayleigh Francis <kayleighrf@gmail.com>
2016-01-16 14:20:18 -0500

    ITimer complete

--- 2016-Stronghold/src/org/fpsrobotics/sensors/ITimer.java
+++ 2016-Stronghold/src/org/fpsrobotics/sensors/ITimer.java
@@ -2,4 +2,16 @@ package org.fpsrobotics.sensors;
 
 public interface ITimer {
 
+	
+	public long getTimeInMillis();
+		
+	
+	
+	public double getTimeInSeconds();
+		
+	
+	
+	public double getTimeInMinutes();
+		
+	
 }
