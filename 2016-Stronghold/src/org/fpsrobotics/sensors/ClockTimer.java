5b8ec6f6be9735d1413a3316416c4dc2afb2fe92
DeanN60 <dneighbour54@gmail.com>
2016-01-16 17:56:23 -0500

    Added unimplemented methods

--- 2016-Stronghold/src/org/fpsrobotics/sensors/ClockTimer.java
+++ 2016-Stronghold/src/org/fpsrobotics/sensors/ClockTimer.java
@@ -2,4 +2,25 @@ package org.fpsrobotics.sensors;
 
 public class ClockTimer implements ITimer {
 
+	@Override
+	public long getTimeInMillis()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getTimeInSeconds()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getTimeInMinutes()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
