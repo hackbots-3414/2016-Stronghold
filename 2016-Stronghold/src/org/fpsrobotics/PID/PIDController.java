9db43fb587408f092c65f85663f3e84861c4d0c6
Kayleigh Francis <kayleighrf@gmail.com>
2016-01-30 13:15:09 -0500

    Updated PID

--- 2016-Stronghold/src/org/fpsrobotics/PID/PIDController.java
+++ 2016-Stronghold/src/org/fpsrobotics/PID/PIDController.java
@@ -10,6 +10,7 @@ public class PIDController extends PIDSubsystem implements IPIDFeedbackDevice{
 	public PIDController(double p , double i, double d) {
 		super("PIDController",2, 0, 0);
 		setSetpoint(d);
+		setAbsoluteTolerance(.2);
 		enable();
 		// TODO Auto-generated constructor stub
 	}
@@ -17,8 +18,7 @@ public class PIDController extends PIDSubsystem implements IPIDFeedbackDevice{
 	public static final double p = .1;
 	public static final double i = .01;
 	public static final double d = .001;
-	private final AnalogInput pot = new AnalogInput(2);
-	
+	private final AnalogInput pot = new AnalogInput(1);
 	private final CANTalon motor = new CANTalon(1,2);
 	//private final AnalogChannel pot = new AnalogChannel(4);
 	
